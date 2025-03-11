d6376f3bcc288fbb543aeadfc7550b2122c9182d
PhamLoc9504 <minhloc090504@gmail.com>
2025-03-11 11:17:39 +0700

    [add] Lab03 ui caesar

--- Lab02/api.py
+++ Lab02/api.py
@@ -1,95 +1,94 @@
 from flask import Flask, request, jsonify
-# from cipher.caesar import CaesarCipher
-# from cipher.vigenere import VigenereCipher
-# from cipher.railfence import RailfenceCipher
-from playfair import PlayfairCipher  
-from transposition import TranspositionCipher
+from cipher.caesar import CaesarCipher
+from cipher.vigenere import VigenereCipher
+from cipher.railfence import RailfenceCipher
+from cipher.playfair import PlayfairCipher  
+from cipher.transposition import TranspositionCipher
 app = Flask(__name__)
 
-# # Caesar Cipher
-# caesar_cipher = CaesarCipher()
+# Caesar Cipher
+caesar_cipher = CaesarCipher()
 
-# @app.route('/api/caesar/encrypt', methods=['POST'])
-# def caesar_encrypt():
-#     data = request.get_json()
-#     plain_text = data.get('plain_text')
-#     key = data.get('key')
-#     if not plain_text or not key:
-#         return jsonify({'error': 'Missing plain_text or key'}), 400
-#     try:
-#         key = int(key)
-#     except ValueError:
-#         return jsonify({'error': 'Key must be an integer'}), 400
-#     encrypted_text = caesar_cipher.encrypt_text(plain_text, key)
-#     return jsonify({'encrypted_text': encrypted_text})
+@app.route('/api/caesar/encrypt', methods=['POST'])
+def caesar_encrypt():
+    data = request.get_json()
+    plain_text = data.get('plain_text')
+    key = data.get('key')
+    if not plain_text or not key:
+        return jsonify({'error': 'Missing plain_text or key'}), 400
+    try:
+        key = int(key)
+    except ValueError:
+        return jsonify({'error': 'Key must be an integer'}), 400
+    encrypted_text = caesar_cipher.encrypt_text(plain_text, key)
+    return jsonify({'encrypted_text': encrypted_text})
 
-# @app.route('/api/caesar/decrypt', methods=['POST'])
-# def caesar_decrypt():
-#     data = request.get_json()
-#     cipher_text = data.get('cipher_text')
-#     key = data.get('key')
-#     if not cipher_text or not key:
-#         return jsonify({'error': 'Missing cipher_text or key'}), 400
-#     try:
-#         key = int(key)
-#     except ValueError:
-#         return jsonify({'error': 'Key must be an integer'}), 400
-#     decrypted_text = caesar_cipher.decrypt_text(cipher_text, key)
-#     return jsonify({'decrypted_text': decrypted_text})
+@app.route('/api/caesar/decrypt', methods=['POST'])
+def caesar_decrypt():
+    data = request.get_json()
+    cipher_text = data.get('cipher_text')
+    key = data.get('key')
+    if not cipher_text or not key:
+        return jsonify({'error': 'Missing cipher_text or key'}), 400
+    try:
+        key = int(key)
+    except ValueError:
+        return jsonify({'error': 'Key must be an integer'}), 400
+    decrypted_text = caesar_cipher.decrypt_text(cipher_text, key)
+    return jsonify({'decrypted_text': decrypted_text})
 
-# # Vigenere Cipher
-# vigenere_cipher = VigenereCipher()
+# Vigenere Cipher
+vigenere_cipher = VigenereCipher()
 
-# @app.route('/api/vigenere/encrypt', methods=['POST'])
-# def vigenere_encrypt():
-#     data = request.get_json()
-#     plain_text = data.get('plain_text')
-#     key = data.get('key')
-#     if not plain_text or not key:
-#         return jsonify({'error': 'Missing plain_text or key'}), 400
-#     encrypted_text = vigenere_cipher.vigenere_encrypt(plain_text, key)
-#     return jsonify({'encrypted_text': encrypted_text})
+@app.route('/api/vigenere/encrypt', methods=['POST'])
+def vigenere_encrypt():
+    data = request.get_json()
+    plain_text = data.get('plain_text')
+    key = data.get('key')
+    if not plain_text or not key:
+        return jsonify({'error': 'Missing plain_text or key'}), 400
+    encrypted_text = vigenere_cipher.vigenere_encrypt(plain_text, key)
+    return jsonify({'encrypted_text': encrypted_text})
 
-# @app.route('/api/vigenere/decrypt', methods=['POST'])
-# def vigenere_decrypt():
-#     data = request.get_json()
-#     cipher_text = data.get('cipher_text')
-#     key = data.get('key')
-#     if not cipher_text or not key:
-#         return jsonify({'error': 'Missing cipher_text or key'}), 400
-#     decrypted_text = vigenere_cipher.vigenere_decrypt(cipher_text, key)
-#     return jsonify({'decrypted_text': decrypted_text})
+@app.route('/api/vigenere/decrypt', methods=['POST'])
+def vigenere_decrypt():
+    data = request.get_json()
+    cipher_text = data.get('cipher_text')
+    key = data.get('key')
+    if not cipher_text or not key:
+        return jsonify({'error': 'Missing cipher_text or key'}), 400
+    decrypted_text = vigenere_cipher.vigenere_decrypt(cipher_text, key)
+    return jsonify({'decrypted_text': decrypted_text})
 
-# # Railfence Cipher
-# railfence_cipher = RailfenceCipher()
+railfence_cipher = RailfenceCipher()
 
-# @app.route('/api/railfence/encrypt', methods=['POST'])
-# def railfence_encrypt():
-#     data = request.get_json()
-#     plain_text = data.get('plain_text')
-#     key = data.get('key')
-#     if not plain_text or not key:
-#         return jsonify({'error': 'Missing plain_text or key'}), 400
-#     try:
-#         key = int(key)
-#     except ValueError:
-#         return jsonify({'error': 'Key must be an integer'}), 400
-#     encrypted_text = railfence_cipher.rail_fence_encrypt(plain_text, key)
-#     return jsonify({'encrypted_text': encrypted_text})
+@app.route('/api/railfence/encrypt', methods=['POST'])
+def railfence_encrypt():
+     data = request.get_json()
+     plain_text = data.get('plain_text')
+     key = data.get('key')
+     if not plain_text or not key:
+         return jsonify({'error': 'Missing plain_text or key'}), 400
+     try:
+         key = int(key)
+     except ValueError:
+        return jsonify({'error': 'Key must be an integer'}), 400
+     encrypted_text = railfence_cipher.rail_fence_encrypt(plain_text, key)
+     return jsonify({'encrypted_text': encrypted_text})
 
-# @app.route('/api/railfence/decrypt', methods=['POST'])
-# def railfence_decrypt():
-#     data = request.get_json()
-#     cipher_text = data.get('cipher_text')
-#     key = data.get('key')
-#     if not cipher_text or not key:
-#         return jsonify({'error': 'Missing cipher_text or key'}), 400
-#     try:
-#         key = int(key)
-#     except ValueError:
-#         return jsonify({'error': 'Key must be an integer'}), 400
-#     decrypted_text = railfence_cipher.rail_fence_decrypt(cipher_text, key)
-#     return jsonify({'decrypted_text': decrypted_text})
+@app.route('/api/railfence/decrypt', methods=['POST'])
+def railfence_decrypt():
+    data = request.get_json()
+    cipher_text = data.get('cipher_text')
+    key = data.get('key')
+    if not cipher_text or not key:
+        return jsonify({'error': 'Missing cipher_text or key'}), 400
+    try:
+        key = int(key)
+    except ValueError:
+        return jsonify({'error': 'Key must be an integer'}), 400
+    decrypted_text = railfence_cipher.rail_fence_decrypt(cipher_text, key)
+    return jsonify({'decrypted_text': decrypted_text})
 
 # Playfair Cipher
 playfair_cipher = PlayfairCipher()
